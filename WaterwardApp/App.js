dc2763dd043eff9cc15238f95dae9d7eea38e634
abdallah <abdallh1133224466@gmail.com>
2024-03-19 14:44:28 +0300

    first commit seting up the devlopment enviroment

---
+++ WaterwardApp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>In the name Of God We Start! Allah Akbar !!!</Text>
+      <Text>     come On !!   </Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
